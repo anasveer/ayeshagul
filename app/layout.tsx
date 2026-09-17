d62e5d06cbd878084fc416955f02239df601111e
Anas <anasaslam461@gmail.com>
2026-09-17 19:10:37 +0500

    .env

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { CartProvider } from "./cart-context";
 const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700", "800"],
+  weight: ["300", "400", "500", "600", "700",],
   display: "swap",
 });
 
